7ef8d045e3b456e28fcd83d59ca4ffb6817c895e
Lcorvle <xsx1996@163.com>
2017-07-16 16:56:31 +0800

    fill function split_feature and function get_feature_distribution

--- scripts/algorithm.py
+++ scripts/algorithm.py
@@ -22,6 +22,11 @@ def split_feature(feature_values: np.array, bin_count: int) -> np.array:
 	'''
 	# TODO
 	bins = []
+	feature_values.sort()
+	size = feature_values.size
+	for i in range(bin_count):
+		bins.append(feature_values[int(i * size / bin_count)])
+	bins.append(feature_values[size - 1])
 	return bins
 
 
@@ -34,5 +39,25 @@ def get_feature_distribution(feature_values: np.array, bins: np.array) -> np.arr
     :return: np.array, the histogram of each bin
 	'''
 	# TODO
-	histogram = []
+	size = bins.size
+	histogram = [0] * (size - 1)
+	for x in feature_values:
+		if x == bins[size - 1]:
+			histogram[size - 2] += 1
+			break
+		l = 0
+		r = size - 1
+		while l <= r:
+			temp = int((l + r) / 2)
+			if bins[temp] <= x < bins[temp + 1]:
+				histogram[temp] += 1
+				break
+			elif x < bins[temp]:
+				r = temp - 1
+			else:
+				l = temp + 1
+	temp_val = 0
+	for i in range(size - 1):
+		if bins[i + 1] != bins[temp_val]:
+
 	return histogram
